39a3fd018f390e564f9847f1a518b7606b7c7877
Shubham Loni <shubham.loni07@gmail.com>
2021-07-31 12:54:35 +0530

    Final Changes

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <React.Fragment>
+      <h1 className='x-large text-primary'>Page Not Found.</h1>
+      <p className='large'>This page does not exists. Got to Home page.</p>
+    </React.Fragment>
+  );
+};
+
+export default NotFound;
